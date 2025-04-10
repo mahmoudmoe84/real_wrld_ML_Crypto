372d6bf21183e2e813aa178e7c9eccc8d9bf16b2
mahmoudmoe84 <mahmoud.moawed@gmail.com>
2025-04-10 20:40:20 +0000

    start of kraken api files

---
+++ Services/trader/src/trader/kraken_api.py
@@ -0,0 +1,63 @@
+import json
+from typing import Dict, List , Tuple 
+from pydantic import BaseModel
+from websocket import create_connection 
+from loguru import logger
+
+class Trade(BaseModel):
+    product_id:str
+    price:float
+    quantity: float 
+    timestamp:str 
+
+class KrakenAPI:
+    URL ="wss://ws.kraken.com/v2"
+
+    def __init__(
+        self, 
+        product_id:list[str],
+        
+    ):
+        self.product_id = product_id
+        
+        #create websocket client 
+        self._ws_client = create_connection(self.URL)
+
+        #send initial subscribe message to the websocket
+        self._subscribe()
+
+    def get_trades(self) -> list[Trade]:
+        
+        data:str = self._ws_client.recv()
+        
+        if "heartbeat" in data:
+            logger.info('heartbeat received')
+            return []
+        try: 
+            data = json.loads(data)
+        except json.JSONDecodeError as e:
+               logger.error(f'error decoding json: {e}')
+               return []
+        breakpoint()
+    
+    def _subscribe(self):
+        """
+        subscribes to the websocket and waits for the intial snapshot
+        and the fiven product_ids
+        """
+        self._ws_client.send(
+            json.dumps({
+                "method": "subscribe",
+                "params": {
+                    "channel": "trade",
+                    "symbol": self.product_id,
+                    "snapshot": False
+                }
+            })
+        )
+        #discard first two messages as they contain no data
+        for _ in self.product_id: 
+            _ = self._ws_client.recv()
+            _ = self._ws_client.recv()
+
+        # breakpoint()
--- Services/trader/src/trader/main.py
+++ Services/trader/src/trader/main.py
@@ -1,28 +1,51 @@
-import time
 from quixstreams import Application
+from kraken_api import KrakenAPI , Trade
+from typing import Dict, List , Tuple 
 
 
-app = Application(
-    broker_address='localhost:31234', consumer_group='example'
-)
+
+def run(
+    kafka_broker_address: str,
+    kafka_topic_name: str,
+    kraken_api
+):
+    app = Application(
+        broker_address=kafka_broker_address, 
+        consumer_group='example'
+    )
 
 # Define a topic "my_topic" with JSON serialization
-topic = app.topic(name='my_topic', value_serializer='json')
-
-
-
-while True:
-    event = {"id": "1", "text": "Lorem ipsum dolor sit amet"}
-
-    # Create a Producer instance
+    topic = app.topic(name=kafka_topic_name,
+                    value_serializer='json')
     with app.get_producer() as producer:
+        while True:
+            events:list[Trade] = kraken_api.get_trades()
 
-        # Serialize an event using the defined Topic 
-        message = topic.serialize(key=event["id"], value=event)
-
-        # Produce a message into the Kafka topic
-        producer.produce(
-            topic=topic.name, value=message.value, key=message.key
-        )
         
-        time.sleep(1)
+            # Create a Producer instance
+                # Serialize an event using the defined Topic 
+            
+            for event in events:
+                message = topic.serialize(
+                    # key=event["id"],
+                    value=event)
+
+                # Produce a message into the Kafka topic
+                producer.produce(
+                    topic=topic.name, 
+                    value=message.value, 
+                    # key=message.key
+                )
+                
+
+
+if __name__ == "__main__":
+    api = KrakenAPI(
+        product_id=['BTC/EUR']
+    )
+    
+    run(
+        kafka_broker_address='localhost:31234',
+        kafka_topic_name='trades',
+        kraken_api=api
+    )
